d6528e70e8e7ded3ec7f7cdb242fa77bd780a9d3
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-13 14:40:38 -0700

    created the reducer file

---
+++ reducer-app/src/Reducers/Reducer.js
@@ -0,0 +1,16 @@
+export const initialState = {
+  toDo: [
+    {
+      item: "Feed the neighbor's dogs",
+      completed: false,
+      id: 3892987589
+    }
+  ]
+}
+
+export const Reducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
